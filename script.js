1d4b0888af02a402222303515592c90364773caf
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-07-24 19:50:17 -0700

    finishes part 2

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-var count = 3;
 
-function addOne() {
-    count += 1;
-    document.querySelector(".row span").innerText = `${count} like(s)`;
+function addOne1() {
+    let count = document.querySelector(".row .num1").innerText;
+    count ++;
+    document.querySelector(".row .num1").innerText = `${count}`;
+
+}
+
+function addOne2() {
+    let count = document.querySelector(".row .num2").innerText;
+    count ++;
+    document.querySelector(".row .num2").innerText = `${count}`;
+
+}
+
+function addOne3() {
+    let count = document.querySelector(".row .num3").innerText;
+    count ++;
+    document.querySelector(".row .num3").innerText = `${count}`;
 }
