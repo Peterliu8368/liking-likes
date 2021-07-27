e3f2c463af965c0b23da5208a34a8178d68d07ad
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-07-27 11:09:30 -0700

    cleaner code yay

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
 
-function addOne1() {
-    let count = document.querySelector(".row .num1").innerText;
-    count ++;
-    document.querySelector(".row .num1").innerText = `${count}`;
-
-}
-
-function addOne2() {
-    let count = document.querySelector(".row .num2").innerText;
-    count ++;
-    document.querySelector(".row .num2").innerText = `${count}`;
-
-}
-
-function addOne3() {
-    let count = document.querySelector(".row .num3").innerText;
-    count ++;
-    document.querySelector(".row .num3").innerText = `${count}`;
+function add(num) {
+    let count = document.querySelector(`.row .num${num}`).innerText;
+    count++;
+    document.querySelector(`.row .num${num}`).innerText = `${count}`;
 }
